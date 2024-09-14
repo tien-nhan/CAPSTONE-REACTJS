5fcc64c12c36beeeeb08eda8f17878763c75dc1c
Lam <lamkyo373@gmail.com>
2024-09-14 21:33:03 +0700

    done with logout and the button navigating to the admin page

--- src/redux/models/auth.js
+++ src/redux/models/auth.js
@@ -2,47 +2,50 @@ import { message } from "antd";
 import authProvider from "../../data-access/auth-provider";
 
 export default {
-  state: { auth: JSON.parse(localStorage.getItem("auth") || "{}") },
-  reducers: {
-    // handle state changes with pure functions
-    updateData(state, payload) {
-      return { ...state, ...payload };
-    },
-  },
-  effects: (dispatch) => ({
-    // handle state changes with impure functions.
-    // use async/await for async actions
-    onLogin: (payload, state) => {
-      return new Promise(async (resolve, reject) => {
-        authProvider
-          .login(payload)
-          .then((s) => {
-            message.success("Đăng nhập thành công");
-            dispatch.auth.updateData({ auth: s });
-            localStorage.setItem("access_token", s?.accessToken);
-            localStorage.setItem("auth", JSON.stringify(s));
+	state: { auth: JSON.parse(localStorage.getItem("auth") || "{}") },
+	reducers: {
+		// handle state changes with pure functions
+		updateData(state, payload) {
+			return { ...state, ...payload };
+		},
+		deleteAuth(state, payload) {
+			return { ...state, auth: {} };
+		},
+	},
+	effects: (dispatch) => ({
+		// handle state changes with impure functions.
+		// use async/await for async actions
+		onLogin: (payload, state) => {
+			return new Promise(async (resolve, reject) => {
+				authProvider
+					.login(payload)
+					.then((s) => {
+						message.success("Đăng nhập thành công");
+						dispatch.auth.updateData({ auth: s });
+						localStorage.setItem("access_token", s?.accessToken);
+						localStorage.setItem("auth", JSON.stringify(s));
 
-            resolve(s);
-          })
-          .catch((e) => {
-            message.error(e.message);
-            reject(e);
-          });
-      });
-    },
-    onRegister: (payload, state) => {
-      return new Promise(async (resolve, reject) => {
-        authProvider
-          .register(payload)
-          .then((s) => {
-            message.success("Đăng ký thành công");
-            resolve(s);
-          })
-          .catch((e) => {
-            message.error(e.message);
-            reject(e);
-          });
-      });
-    },
-  }),
+						resolve(s);
+					})
+					.catch((e) => {
+						message.error(e.message);
+						reject(e);
+					});
+			});
+		},
+		onRegister: (payload, state) => {
+			return new Promise(async (resolve, reject) => {
+				authProvider
+					.register(payload)
+					.then((s) => {
+						message.success("Đăng ký thành công");
+						resolve(s);
+					})
+					.catch((e) => {
+						message.error(e.message);
+						reject(e);
+					});
+			});
+		},
+	}),
 };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,150 +14,159 @@ import Contact from "../pages/Contact";
 import News from "../pages/News";
 import Details from "../pages/Details";
 import Booking from "../pages/Booking";
+import Profile from "../pages/Profile";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Homepage />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Contact />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/news"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <News />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/details/:id"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Details />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/booking/:id"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Booking />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Login />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/register"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <Register />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/admin"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <PrivateRoute />
-          </Suspense>
-        }
-      >
-        <Route
-          path="films"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <Films />
-            </Suspense>
-          }
-        />
-        <Route
-          path="films/addnew"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <AddNewFilm />
-            </Suspense>
-          }
-        />
-        <Route
-          path="films/edit/:maPhim"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <AddNewFilm />
-            </Suspense>
-          }
-        />
-        <Route
-          path="films/showtime/:maPhim"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <ShowTime />
-            </Suspense>
-          }
-        />
-        <Route
-          path="quanlynguoidung"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <QuanLyNguoiDung />
-            </Suspense>
-          }
-        />
-        <Route
-          path="quanlynguoidung/addnew"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <AddNewNguoiDung />
-            </Suspense>
-          }
-        />
-        <Route
-          path="quanlynguoidung/edit"
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <AddNewNguoiDung />
-            </Suspense>
-          }
-        />
-      </Route>
-      <Route
-        path="/thongtincanhan"
-        element={
-          <Suspense fallback={<div>loading...</div>}>
-            <PrivateRoute />
-          </Suspense>
-        }
-      >
-        <Route
-          path=""
-          element={
-            <Suspense fallback={<div>loading...</div>}>
-              <Information />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Homepage />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/contact"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Contact />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/news"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<News />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/profile"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Profile />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/details/:id"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Details />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/booking/:id"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Booking />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/login"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Login />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/register"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<Register />
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/admin"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<PrivateRoute />
+					</Suspense>
+				}
+			>
+				<Route
+					path="films"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<Films />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="films/addnew"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<AddNewFilm />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="films/edit/:maPhim"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<AddNewFilm />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="films/showtime/:maPhim"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<ShowTime />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="quanlynguoidung"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<QuanLyNguoiDung />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="quanlynguoidung/addnew"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<AddNewNguoiDung />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="quanlynguoidung/edit"
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<AddNewNguoiDung />
+						</Suspense>
+					}
+				/>
+			</Route>
+			<Route
+				path="/thongtincanhan"
+				element={
+					<Suspense fallback={<div>loading...</div>}>
+						<PrivateRoute />
+					</Suspense>
+				}
+			>
+				<Route
+					path=""
+					element={
+						<Suspense fallback={<div>loading...</div>}>
+							<Information />
+						</Suspense>
+					}
+				/>
+			</Route>
+		</Routes>
+	);
 };
 
 export default AppRoutes;
